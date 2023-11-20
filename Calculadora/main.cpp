90ba15dafccb739a39e89a467dae6941dab02166
Joan Galindo López <joan@flx.cat>
2023-11-20 17:00:43 +0100

    Initial Commit

---
+++ Calculadora/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Calculadora
+//
+//  Created by Joan Galindo López on 20/11/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
