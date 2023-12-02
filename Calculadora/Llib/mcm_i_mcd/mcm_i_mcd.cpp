be1dc4b00fd9e2d23f63062a687c5900443d711e
Joan Galindo López <joan@flx.cat>
2023-12-02 20:45:14 +0100

    9

---
+++ Calculadora/Llib/mcm_i_mcd/mcm_i_mcd.cpp
@@ -0,0 +1,13 @@
+#include "mcm_i_mcd.h"
+
+int mcd(int a, int b) {
+    if (b > a) return mcd(b, a);
+    if (b == 0) return 1;
+    int c = a % b;
+    if (c == 0) return b;
+    return mcd(b, c);
+}
+
+int mcm(int a, int b) {
+    return (a * b) / mcd(a, b);
+}
---
+++ Calculadora/Llib/mcm_i_mcd/mcm_i_mcd.h
@@ -0,0 +1,7 @@
+#ifndef mcm_i_mcd_h
+#define mcm_i_mcd_h
+
+int mcd(int a, int b);
+int mcm(int a, int b);
+
+#endif
